4b2a7dca18a1ea9dbb316897121789aff17b94a6
Steven Peralta <155492295+Pianist01@users.noreply.github.com>
2024-12-30 16:26:08 -0800

    Create singleservice.js

---
+++ singleservice.js
@@ -0,0 +1,96 @@
+console.log('This page is working');
+
+// Logo functionality
+const logo = document.querySelector('.logo');
+logo.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.href = 'index.html';
+});
+
+// Nav menu
+
+let navMenu;
+const navBox = document.querySelector('.icon-container');
+navBox.addEventListener('click', (e) => {
+    navMenu = document.querySelector('.navigation');
+    navMenu.style.width = '300px';
+    navMenu.style.display = 'block';
+});
+
+const navExit = document.querySelector('.nav-exit');
+navExit.addEventListener('click', (e) => {
+    e.preventDefault();
+    navMenu.style.width = '0';
+});
+
+let tuesday = document.createElement('a');
+let cena = document.createElement('a');
+let vigil = document.createElement('a');
+
+const serviceLink = [tuesday, cena, vigil];
+
+const serviceMenu = document.querySelector('.service');
+const maxHeight = 60;
+const baseHeight = 25;
+
+function dropDownUp() {
+    serviceMenu.addEventListener('mouseenter', (e) => {
+        e.preventDefault();
+
+        height = 25;
+        maxHeight;
+
+        const animateDrop = () => {
+            height += 3;
+
+            serviceMenu.style.height = height + '%';
+            serviceMenu.style.gridTemplateRows = 'repeat(5, .3fr)';
+            if(height < maxHeight) {
+                requestAnimationFrame(animateDrop);
+            }
+        }
+
+        serviceLink.forEach((service, index) => {
+            if(index === 0) {
+                service.href = '#';
+                service.textContent = 'Martes';
+                service.classList.add('sunday');
+                service.style.display = 'block';
+            } else if(index === 1) {
+                service.href = '#';
+                service.textContent = 'Santa Cena';
+                service.classList.add('tuesday');
+                service.style.display = 'block';
+            } else if(index === 2) {
+                service.href = '#';
+                service.textContent = 'Vigilia';
+                service.classList.add('cena');
+                service.style.display = 'block';
+            } 
+            serviceMenu.append(service);
+        });
+
+        animateDrop();
+    })
+
+    serviceMenu.addEventListener('mouseleave', (e) => {
+        e.preventDefault();
+
+        const goAway = () => {
+            height -= 3;
+            serviceMenu.style.height = height + '%';
+            serviceMenu.style.gridTemplateRows = '';
+            if(height > baseHeight) {
+                requestAnimationFrame(goAway);
+            }
+        }
+
+        serviceLink.forEach((item) => {
+            item.style.display = 'none';
+        });
+
+        goAway();
+    })
+}
+
+dropDownUp();
