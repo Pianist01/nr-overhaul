a2e5b45d137a70ae0805a924b24c257e674adc24
Steven Peralta <155492295+Pianist01@users.noreply.github.com>
2026-05-12 10:39:50 -0700

    Refactor levaBox and memberBox styles for mobile
    Updated styles for levaBox and memberBox to improve mobile responsiveness.

--- about.js
+++ about.js
@@ -197,14 +197,28 @@ levaWife.addEventListener('click', (e) => {
 
     levaExit();
 
-    levaBox.style.gridColumnStart = 1;
-    levaBox.style.gridColumnEnd = 4;
-    levaBox.style.gridRowStart = 1;
-    levaBox.style.gridRowEnd = 4;
-    levaBox.style.zIndex = 1;
+    // This is orginal code
+
+    // levaBox.style.gridColumnStart = 1;
+    // levaBox.style.gridColumnEnd = 4;
+    // levaBox.style.gridRowStart = 1;
+    // levaBox.style.gridRowEnd = 4;
+    // levaBox.style.zIndex = 1;
+    // levaBox.style.position = 'absolute';
+    // levaBox.style.justifySelf = 'center';
+    // levaBox.style.alignSelf = 'center';
+
+    // This is new code to see if it will work better for mobile devices
+
+    levaBox.style.height = 'auto';
+    levaBox.style.maxHeight = '75vh';
+    levaBox.style.overflow = 'auto';
     levaBox.style.position = 'absolute';
-    levaBox.style.justifySelf = 'center';
-    levaBox.style.alignSelf = 'center';
+    levaBox.style.top = '60px';
+    levaBox.style.left = '50%';
+    levaBox.style.transform = 'translateX(-50%)';
+    levaBox.style.width = '90%';
+    levaBox.style.zIndex = '10';
 
     levaArea.append(levaBox);
 });
@@ -313,12 +327,30 @@ function animate() {
 
     animateHeight();
 
+    // This is orginal code
+
+    // memberBox.style.gridColumnStart = '1';
+    // memberBox.style.gridColumnEnd = '4';
+    // memberBox.style.gridRowStart = '1';
+    // memberBox.style.gridRowEnd = '3';
+    // memberBox.style.zIndex = '1';
+    // memberBox.style.position = 'absolute';
+
+    // This is new code to see if it will work better for mobile devices
+
+    memberBox.style.height = 'auto';
+    memberBox.style.maxHeight = '75vh';
+    memberBox.style.overflow = 'auto';
     memberBox.style.gridColumnStart = '1';
     memberBox.style.gridColumnEnd = '4';
     memberBox.style.gridRowStart = '1';
     memberBox.style.gridRowEnd = '3';
-    memberBox.style.zIndex = '1';
+    memberBox.style.zIndex = '10';
     memberBox.style.position = 'absolute';
+    memberBox.style.top = '60px';
+    memberBox.style.left = '50%';
+    memberBox.style.transform = 'translateX(-50%)';
+    memberBox.style.width = '90%';
     
 }
 
